2e3be83be4345c2c012a8d1fac8036122deb2305
Tuhin810 <tuhin.thakur1233@gmail.com>
2025-03-11 21:27:47 +0530

    style: format code for consistency and improve server startup message

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,17 @@ import cors from "cors";
 import bodyParser, { json } from "body-parser";
 import connectDb from "./config/db.config";
 
-
 // dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 8989;
 
 const options: cors.CorsOptions = {
-  allowedHeaders: ["sessionId", "Content-Type"],
-  exposedHeaders: ["sessionId"],
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false
+	allowedHeaders: ["sessionId", "Content-Type"],
+	exposedHeaders: ["sessionId"],
+	origin: "*",
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	preflightContinue: false
 };
 
 app.use(cors(options));
@@ -24,19 +23,13 @@ app.use(json());
 app.use(express.json());
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
-
-  res.send(
-    `<h1>Server running successfully</h1>`
-  )
+	res.send(`<h1>Hobi Server running successfully</h1>`);
 });
 
 app.use("/api/v1", require("./api/v1/routers/routes.index"));
 
-
 connectDb();
 
-
 app.listen(port, () => {
-  console.log(`🚀 Server is running at http://localhost:${port}`);
+	console.log(`🚀 Server is running at http://localhost:${port}`);
 });
-
