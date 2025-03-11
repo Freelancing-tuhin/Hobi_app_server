6e097efba08963ef14fd2032666145f7f1d30e88
Tuhin810 <tuhin.thakur1233@gmail.com>
2025-03-11 21:15:29 +0530

    feat: update package.json to use nodemon for development and add nodemon as a dependency; remove unused call routes

--- src/api/v1/routers/call/call.routes.ts
+++ src/api/v1/routers/call/call.routes.ts
@@ -1,22 +1,5 @@
 import { Router } from "express";
-import {
-	acceptCallRequest,
-	cancelCallRequest,
-	completeCall,
-	requestCall,
-	walletUpdateCall
-} from "../../controllers/call/call.controller";
 
 const router = Router();
-
-router.post("/request", requestCall);
-
-router.patch("/accept", acceptCallRequest);
-
-router.patch("/cancel", cancelCallRequest);
-
-router.patch("/complete", completeCall);
-
-router.patch("/wallet_update_call", walletUpdateCall);
 
 module.exports = router;
